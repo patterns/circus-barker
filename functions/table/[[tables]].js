9570e757cd17b946d8cb5aa967e5c57268b5ddb1
興怡 <>
2023-08-05 01:40:56 -0700

    console.log

--- functions/table/[[tables]].js
+++ functions/table/[[tables]].js
@@ -1,10 +1,12 @@
 // forward GET to origin server unless KV record exists
 import { readKVFirst } from "../../src/kv.js"
 
-const read = ({request, env}) => {
-    // adjust the destination server
+export async function onRequestGet(context) {
+  const {request, env } = context;
+  console.log(`RCV GET: ${request.url}`);
+  // adjust the destination server
     const url = new URL(request.url);
     url.hostname = env.ORIGIN_SERVER;
     return readKVFirst(env, url.toString());
 };
-export const onRequestGet = [read];
+
