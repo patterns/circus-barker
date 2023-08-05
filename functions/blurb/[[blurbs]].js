d410c1a14d9d2deb36134302fab19ca577e13461
興怡 <>
2023-08-05 00:39:46 -0700

    try import from src/kv.js

--- functions/blurb/[[blurbs]].js
+++ functions/blurb/[[blurbs]].js
@@ -1,12 +1,10 @@
+// forward GET to origin server unless KV record exists
+import { readKVFirst } from "../../src/kv.js"
 
-import { readKVFirst } from "../../static/kv.js"
-
-export default {
-  async fetch(request, env) {
+const read = ({request, env}) => {
     // adjust the destination server
     const url = new URL(request.url);
     url.hostname = env.ORIGIN_SERVER;
     return readKVFirst(env, url.toString());
-  }
-}
-
+};
+export const onRequestGet = [read];
