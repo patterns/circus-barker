14d4f54b538254595164eb6af1c0d7e5e1844a74
興怡 <>
2023-08-05 21:11:36 -0700

    posts

---
+++ functions/inbox.js
@@ -0,0 +1,25 @@
+// forward POST request to origin 
+
+export async function onRequestPost(context) {
+  const { request, env } = context;
+  // specify fields on new constructor
+  const newRequestInit = {
+      redirect: "follow",
+  };
+  // adjust the destination server
+  const url = new URL(request.url);
+  url.hostname = env.ORIGIN_SERVER;
+  const newRequest = new Request(
+      url.toString(),
+      new Request(request, newRequestInit),
+  );
+  try {
+      return await fetch(newRequest);
+  } catch (e) {
+      return new Response(JSON.stringify({ error: e.message }), {
+        status: 500,
+      });
+  }
+}
+
+
